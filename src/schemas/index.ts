4e872db4cc37fe770ada0faa4acccad723964f60
hammoh7 <soahammohaadkar@gmail.com>
2024-02-04 00:58:39 +0530

    Schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
         message: "Password required"
     })
 })
+
+export const RegisterSchema = z.object({
+    name: z.string().min(1, {
+        message: "Name required"
+    }),
+    email: z.string().email({
+        message: "Email required"
+    }),
+    password: z.string().min(6, {
+        message: "Minimum 6 character needed"
+    })
+})
