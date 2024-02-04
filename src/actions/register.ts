7489aac41db2f7b6c449318d3cd71358b21bac81
hammoh7 <soahammohaadkar@gmail.com>
2024-02-04 15:37:42 +0530

    Actions

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,12 +1,33 @@
 "use server";
 
 import { RegisterSchema } from "@/schemas";
-import * as z  from "zod";
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { database } from "@/lib/database";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
-    if (!validatedFields.success) {
-        return { error: "Invalid field!" }
-    }
-    return { success: "Verified successfully!" }
-}
+  const validatedFields = RegisterSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid field!" };
+  }
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already exists!" };
+  }
+
+  await database.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // To do: Sending verification email after registering
+
+  return { success: "Verified successfully!" };
+};
